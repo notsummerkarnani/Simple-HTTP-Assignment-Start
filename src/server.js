5307d6454e8c5a9c20a891b1a0cc8c5dc4204385
notsummerkarnani <93565277+notsummerkarnani@users.noreply.github.com>
2022-01-23 16:07:23 -0500

    trying again

--- src/server.js
+++ src/server.js
@@ -7,36 +7,37 @@ const imageHandler = require('./imageResponses');
 const port = process.env.port || process.env.NODE_PORT || 3000;
 
 const onRequest = (request, response) => {
-  console.log(request.url);
+    console.log(request.url);
 
-  switch (request.url) {
-    case '/':
-      htmlHandler.getIndex(request, response);
-      break;
-    case '/page2':
-      htmlHandler.getPage2(request, response);
-      break;
-    case '/hello':
-      textHandler.getHello(request, response);
-      break;
-    case '/time':
-      textHandler.getTime(request, response);
-      break;
-    case '/helloJSON':
-      jsonHandler.getHelloJSON(request, response);
-      break;
-    case '/timeJSON':
-      jsonHandler.getTimeJSON(request, response);
-      break;
-    case '/dankmemes':
-      imageHandler.getImage(request, response);
-      break;
-    default:
-      htmlHandler.getIndex(request, response);
-      break;
-  }
+    switch (request.url) {
+        case '/':
+            htmlHandler.getIndex(request, response);
+            break;
+        case '/page2':
+            htmlHandler.getPage2(request, response);
+            break;
+        case '/hello':
+            textHandler.getHello(request, response);
+            break;
+        case '/time':
+            textHandler.getTime(request, response);
+            break;
+        case '/helloJSON':
+            jsonHandler.getHelloJSON(request, response);
+            break;
+        case '/timeJSON':
+            jsonHandler.getTimeJSON(request, response);
+            break;
+        case '/dankmemes':
+            imageHandler.getImage(request, response);
+            break;
+        default:
+            htmlHandler.getIndex(request, response);
+            break;
+    }
 };
 
 http.createServer(onRequest).listen(port, () => {
-  console.log(`Listening on port 127.0.0.1:${port}`);
+    console.log(`Listening on port 127.0.0.1:${port}`);
+
 });

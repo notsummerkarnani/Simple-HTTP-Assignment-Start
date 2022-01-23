1af95e3f60e8070a5d402e3f90ba62148b591a50
notsummerkarnani <93565277+notsummerkarnani@users.noreply.github.com>
2022-01-23 15:13:16 -0500

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const htmlHandler = require('./htmlResponses');
+const jsonHandler = require('./jsonResponses');
+const textHandler = require('./textResponses');
+const imageHandler = require('./imageResponses');
+
+const port = process.env.port || process.env.NODE_PORT || 3000;
+
+const onRequest = (request, response) => {
+  console.log(request.url);
+
+  switch (request.url) {
+    case '/':
+      htmlHandler.getIndex(request, response);
+      break;
+    case '/page2':
+      htmlHandler.getPage2(request, response);
+      break;
+    case '/hello':
+      textHandler.getHello(request, response);
+      break;
+    case '/time':
+      textHandler.getTime(request, response);
+      break;
+    case '/helloJSON':
+      jsonHandler.getHelloJSON(request, response);
+      break;
+    case '/timeJSON':
+      jsonHandler.getTimeJSON(request, response);
+      break;
+    case '/dankmemes':
+      imageHandler.getImage(request, response);
+      break;
+    default:
+      htmlHandler.getIndex(request, response);
+      break;
+  }
+};
+
+http.createServer(onRequest).listen(port, () => {
+  console.log(`Listening on port 127.0.0.1:${port}`);
+});
